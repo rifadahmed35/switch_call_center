5083a851d41115e347d418c817a7acde797ec825
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-08 12:10:51 +0100

    Switch assignment: call center

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int x;
+        x=2;
+        switch (x){
+            case 1:
+                System.out.println("German");
+                break;
+            case 2:
+                System.out.println("English");
+                break;
+            case 3:
+                System.out.println("French");
+        }
+    }
+}
